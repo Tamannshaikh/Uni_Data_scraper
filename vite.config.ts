073a5fe233e05ef1f64e3a2139a2ffa9bd68845d
Tamanna Shaikh <shaikhtamanna678@gmail.com>
2026-07-06 22:48:14 +0530

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart({
-      server: { entry: "server" },
+      server: {
+        preset: "vercel",
+        entry: "server",
+      },
     }),
-    nitro(),
     react(),
   ],
   server: {
